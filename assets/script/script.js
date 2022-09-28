3c98c7f43ca0644a140225a7a4129ec8324eac41
vantam8300 <vantam8300@gmail.com>
2022-09-27 22:40:14 -0700

    changed the solution

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,74 +1,99 @@
 // Assignment code here
-function generatePassword() {
+var specialCharacter = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.']
+
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+function getPasswordOption() {
   // prompt user for the length
-  var length = window.prompt("Please enter the length of your password from at least 8 to 128 characters");
+  var length = parseInt(window.prompt("Please enter the length of your password from at least 8 to 128 characters"));
 
+  if (Number.isNaN(length)) {
+    alert("password length must be provided as a number");
+    return null;
+  }
   // validate the length of password
-  while (length < 8 || length > 128) {
-    length = window.prompt("Please enter the length of your password from at least 8 to 128 characters");
+  if (length < 8 || length > 128) {
+    alert("password must be between 8 to 128 characters");
+    return null;
   }
 
-  // call function to get character types
-  var characterTypes = promptCharaterTypes();
+  // prompt user for character types
+  var hasLowercase = window.confirm("Do you want your password including lowercase characters?");
 
-  var characters = "";
+  var hasUppercase = window.confirm("Do you want your password including uppercase characters?");
 
-  // if user want to inclure lowercase
-  if (characterTypes[0]) {
-    characters += "abcdefghijklmnopqrstuvwxyz"
+  var hasNumeric = window.confirm("Do you want your password including numeric characters?");
+
+  var hasSpecial = window.confirm("Do you want your password including special characters?");
+
+  // validate character type
+  if (hasLowercase === false && hasUppercase === false && hasNumeric === false && hasSpecial === false)  {
+    alert("must select at least one character type");
+    return null;
   }
 
-  //if user want to inclure uppercase
-  if (characterTypes[1]) {
-    characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var passwordOption = {
+    length: length,
+    hasLowercase: hasLowercase,
+    hasUppercase: hasUppercase,
+    hasNumeric: hasNumeric,
+    hasSpecial: hasSpecial
+  };
+
+ return passwordOption;
+}
+
+
+function getRandom(arr) {
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement;
+}
+
+function generatePassword(){
+
+  var characterTypes = getPasswordOption();
+  var result = [];
+  var guaranteedChars = [];
+  var posibleCharacter = [];
+
+  // password has at least one lowercase
+  if (characterTypes.hasLowercase) {
+    posibleCharacter = posibleCharacter.concat(lowerCase);
+    guaranteedChars.push(getRandom(lowerCase));
   }
 
-  // if user want to inclure numeric
-  if (characterTypes[2]) {
-    characters += "0123456789"
+  // password has at least one uppercase
+  if (characterTypes.hasUppercase) {
+    posibleCharacter = posibleCharacter.concat(uppercase);
+    guaranteedChars.push(getRandom(uppercase));
   }
 
-  // if user want to inclure special characters
-  if (characterTypes[3]) {
-    characters += " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  // password has at least one number
+  if (characterTypes.hasNumeric) {
+    posibleCharacter = posibleCharacter.concat(number);
+    guaranteedChars.push(getRandom(number));
   }
-  
-  // pick random letter from "characters" and add to result
-  var result = "";
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-    characters.length));
+
+  // password has at least one special character
+  if (characterTypes.hasSpecial) {
+    posibleCharacter = posibleCharacter.concat(specialCharacter);
+    guaranteedChars.push(getRandom(specialCharacter));
   }
+
+  // fill the rest with random character
+  for (var i = guaranteedChars.length; i < characterTypes.length; i++) {
+    guaranteedChars.push(getRandom(posibleCharacter));
+  }
+
+  result = guaranteedChars.join("");
 
   return result;
 
-}
-
-// prompt user for character types
-function promptCharaterTypes() {
-
-  var lowercase = window.confirm("Do you want your password including lowercase characters?");
-
-  var uppercase = window.confirm("Do you want your password including uppercase characters?");
-
-  var numeric = window.confirm("Do you want your password including numeric characters?");
-
-  var special = window.confirm("Do you want your password including special characters?");
-
-  var types = [lowercase, uppercase, numeric, special];
-
-  // validate if user select at least one character type
-  for (var i = 0; i < types.length; i++) {
-    if (types[i]) {
-      break;
-    } else if (i == 3 && types[i] == false) {
-      // ask user to re-enter character types
-      window.alert("Please select at least one character type");
-      promptCharaterTypes()
-    }
-  }
-
-  return types;
 }
 
 // Get references to the #generate element
